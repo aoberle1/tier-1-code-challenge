df90c13c5c5cdba43c11e6315aafe7cc0debd0af
Adam Oberle <oberle.adam@gmail.com>
2023-05-01 18:19:17 -0500

    added myScore variable outside function, compared myScore variable to highScore variable, set the value of high score equal to the greater value using if statement, console logged updated value of high score

--- client.js
+++ client.js
@@ -65,22 +65,40 @@ addTile( 'F', 4 );
 console.log( ...myScrabbleTiles )
 
 // 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
+  // creating myScore variable starting at zero
+let myScore = 0;
+
 function sumTiles( anArray ) {
   let totalScore = 0
+  // for loop to iterate through array
   for (i = 0; i < myScrabbleTiles.length; i++ ){
+  // score of each tile will be added to the value of totalScore at each iteration of the loop
   totalScore = totalScore + myScrabbleTiles[i].score;
   }
-  return totalScore 
+  // setting value of myScore to equal totalScore (sum total of tile scores)
+  let myScore = totalScore
+  // if statement to determine if sum of all tiles (myScore) is greater than high score,
+  // which changes the value of high score to the value of myScore if true
+  // and announces that we have the new high score, and what the new high score is
+  if ( myScore > highScore ){
+    highScore = myScore;
+    console.log( `We have the new high score which is ${highScore}!` )
+  }
+  // returns sum total of scores of all tiles
+  return totalScore;
+  
 }
 
-console.log( sumTiles( myScrabbleTiles ) );
-
+// console log uses sumTiles function to log the sum total of all tiles in myScrabbleTiles array
+console.log( 'I have', sumTiles( myScrabbleTiles ), 'points!' );
 
 // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
-
-
+// created myScore variable set to 0 on line 69, set the value equal to 0 outside of function,
+// set value of myScore equal to totalScore inside function
 // 8. Check whether or not your score is higher than the highScore.
+// using if statement on line 83 inside function to determine if myScore is greater than highScore,
+// if true, sets value of highScore equal to myScore
 // 8-1 If your score is higher, change highScore to the new high score.
-
+console.log( `The high score is ${highScore}`);
 
 // DONE!
